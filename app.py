6f0218d0e412beb2c6a0445b2778144e538435f2
Abdullah <heighter79@gmail.com>
2023-12-07 17:07:56 +0500

    day 1

--- app.py
+++ app.py
@@ -1,12 +1,23 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return render_template('index.html')
+    return render_template('contact.html')
+
+
+@app.route("/login", methods=["POST"])
+def data_receive():
+        name = request.form["username"]
+        password = request.form["password"]
+        return f"<h1>name: {name}</h1>"
+
+@app.route('/contact', methods=["POST"])
+def contact_form():
+    pass
+
 
 
 if __name__ == '__main__':
